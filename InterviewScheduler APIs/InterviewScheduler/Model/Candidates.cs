07f37d3f23912893d9a8cc45d9f156089869f605
Navanishwara Rao Malave <120970227+NaveenMalave@users.noreply.github.com>
2025-04-06 22:26:27 +0530

    Add files via upload

---
+++ InterviewScheduler APIs/InterviewScheduler/Model/Candidates.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewScheduler.Model
+{
+    [Table("Candidates")]
+    public class Candidates
+    {
+        [Key]
+        [Column("CandidateId")]
+        public int CandidateId { get; set; }
+        [Column("CandidateName")]
+        public string CandidateName { get; set; }
+        [Column("emailId")]
+        public string emailId { get; set; }
+        [Column("resume")]
+        public byte[] resume { get; set; }
+
+    }
+}
