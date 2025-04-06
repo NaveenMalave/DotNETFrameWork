3ecdfdbebe7eda0efbb2319fc2c10bfc3cf7f7f6
Navanishwara Rao Malave <120970227+NaveenMalave@users.noreply.github.com>
2025-04-06 22:29:06 +0530

    Add files via upload

---
+++ InterviewScheduler/InterviewScheduler/Controllers/GetCandidateController.cs
@@ -0,0 +1,50 @@
+﻿using InterviewScheduler.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewScheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetCandidateController : ControllerBase
+    {
+        private readonly IInterviewDataAccess dal;
+        public GetCandidateController(IInterviewDataAccess dal)
+        {
+            this.dal = dal;
+        }
+        [HttpGet]
+        [Route("GetByName/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            return Ok(dal.GetByName(name));
+
+        }
+        [HttpGet]
+        [Route("GetByEmail/{email}")]
+        public IActionResult GetByEmail(string email)
+        {
+            try
+            {
+                return Ok(dal.GetByEmailid(email));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("GetByPhone/{phone}")]
+        public IActionResult GetByPhone(string phone)
+        {
+            try
+            {
+                return Ok(dal.GetByPhone(phone));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ InterviewScheduler/InterviewScheduler/Controllers/ScheduleInterviewController.cs
@@ -0,0 +1,30 @@
+﻿using InterviewScheduler.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewScheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduleInterviewController : ControllerBase
+    {
+        private readonly IInterviewDataAccess dal;
+        public ScheduleInterviewController(IInterviewDataAccess dal)
+        {
+            this.dal = dal;
+        }
+        [HttpPost]
+        public IActionResult ScheduleInterview([FromBody] Recruiter recruiter)
+        {
+            try
+            {
+                dal.AddInterview(recruiter);
+                return Ok("Interview is Scheduled");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
